7cc5cd27ec0a602f795d45872ab3ed16b5c8847e
hiep <hieptran22182000@gmail.com>
2021-07-18 01:48:23 +0700

    update

--- src/components/googleMap/mapDevice.jsx
+++ src/components/googleMap/mapDevice.jsx
@@ -5,7 +5,7 @@ const MapDevice = () => {
     <div className="map">
       <iframe
         width="100%"
-        height="450"
+        height="600"
         title="map"
         loading="lazy"
         allowFullScreen
--- src/pages/newProduct/NewProduct.jsx
+++ src/pages/newProduct/NewProduct.jsx
@@ -3,28 +3,17 @@ import "./newProduct.css";
 export default function NewProduct() {
   return (
     <div className="newProduct">
-      <h1 className="addProductTitle">New Product</h1>
+      <h1 className="addProductTitle">Thêm thiết bị mới</h1>
       <form className="addProductForm">
         <div className="addProductItem">
-          <label>Image</label>
-          <input type="file" id="file" />
+          <label>Mã thiết bị</label>
+          <input type="text" />
         </div>
         <div className="addProductItem">
-          <label>Name</label>
-          <input type="text" placeholder="Apple Airpods" />
-        </div>
-        <div className="addProductItem">
-          <label>Stock</label>
+          <label>Hệ thống cục bộ</label>
           <input type="text" placeholder="123" />
         </div>
-        <div className="addProductItem">
-          <label>Active</label>
-          <select name="active" id="active">
-            <option value="yes">Yes</option>
-            <option value="no">No</option>
-          </select>
-        </div>
-        <button className="addProductButton">Create</button>
+        <button className="addProductButton">Thêm mới</button>
       </form>
     </div>
   );
