8aca200c9a6a46e7d926fd55d6b18c577d1d0029
hiep <hieptran22182000@gmail.com>
2022-04-11 19:03:27 +0700

    F

--- src/runapp.sh
+++ src/runapp.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-
-
-# PID=`ps -eaf | grep python3 app.py | grep -v grep | awk '{print $2}'`
-# if [[ "" !=  "$PID" ]]; then
-#   echo "killing $PID"
-#   kill -9 $PID
-# fi
-
-currentDate=`date`
-echo $currentDate
-
+while true
+do
+   currentDate=`date +"%T"`
+   echo $currentDate
+   # cd ./backend
+   PID=`ps -eaf | grep python3 app.py | grep -v grep | awk '{print $2}'`
+   echo $PID
+#    if [[ "" !=  "$PID" ]]; then
+#       echo "killing $PID"
+#       kill -9 $PID
+#    fi
+done
 
 # ps -ef | grep python3 app.py | grep -v grep | awk '{print $2}' | xargs kill
 
-# cd ./backend
+
 
 # nohup python3 app.py &
