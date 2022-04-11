5bcf407e3038334a20dee60a75f55db1b5eab37f
hiep <hieptran22182000@gmail.com>
2022-04-11 19:14:33 +0700

    f

--- src/backend/runapp.sh
+++ src/backend/runapp.sh
@@ -4,13 +4,13 @@ while true
 do
    currentDate=`date +"%T"`
    echo $currentDate
-   if [[ "$currentDate" ==  "19:11:00" ]]; then
+   if [[ "$currentDate" ==  "19:15:00" ]]; then
       echo "========schedule restart backend=========="
       PID=`lsof -i :5000 | awk '{print $2}'`
       echo $PID
       if [[ "" !=  "$PID" ]]; then
        echo "killing $PID"
-       kill -9 $PID
+      #  kill -9 $PID
        nohup python3 app.py &
       fi
    fi
