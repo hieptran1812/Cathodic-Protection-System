611372cde68666e7c688acd0f3fad748d56338fb
hiep <hieptran22182000@gmail.com>
2022-04-12 11:49:51 +0700

    g

--- src/backend/app.py
+++ src/backend/app.py
@@ -263,7 +263,9 @@ def getChartPortMax():
 def getDashboard():
   devices = []
   # print(currentUser['role'])
-  for doc in db.RectifierTransformersDetails.find({}):
+  for doc in db.RectifierTransformersDetails.find({}, {'_id':1,'devSerial':1,'maChuoi':1, 'organization': 1, 
+                                                       'dateUpdate':1, 'date':1,
+                                                       'otherInfo': {'$slice': 1}}).sort([( '$natural', 1 )]):
     if(currentUser['role'] == 'superadmin'):
       status = (datetime.datetime.now() - doc['otherInfo'][0]['time']).total_seconds()
       if (status >= 300):
@@ -299,7 +301,9 @@ def getDashboard():
           'maChuoi': doc['maChuoi'],
           'status': status
         })
-  for doc in db.TestPostsDetails.find({}):
+  for doc in db.TestPostsDetails.find({}, {'_id':1,'devSerial':1,'maChuoi':1, 'organization': 1, 
+                                                       'dateUpdate':1, 'date':1,
+                                                       'otherInfo': {'$slice': 1}}).sort([( '$natural', 1 )]):
     if(currentUser['role'] == 'superadmin'):
       status = (datetime.datetime.now() - doc['otherInfo'][0]['time']).total_seconds()
       if (status >= 300):
